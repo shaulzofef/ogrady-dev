13a0c64a2687d518a3c684766600bbdcab806ab4
shaulzofef <shaulzofef@gmail.com>
2023-08-14 17:17:42 +0300

    General Cleanup

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-		let navWrapper = document.querySelector('.nav-wrapper'),
-    navMid = document.querySelector('.nav-mid'),
-	navToogler =  document.querySelector('.nav-toogler')
-
-navToogler.addEventListener('click', function (event) {
-	navWrapper.classList.toggle('active'),
-  navMid.classList.toggle('active')
-})
